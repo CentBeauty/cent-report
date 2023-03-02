a7dcdf2f60a0eb8f38aab584b0e5f96bbaf58bca
anonymous <mangnb30062001@gmail.com>
2023-03-02 17:12:40 +0700

    update

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -4,6 +4,7 @@ export default function Homepage() {
     useEffect(() => {
         document.title = 'Home page';
     }, [])
+    console.log("import.meta.env.VITE_PORT",import.meta.env.VITE_PORT)
     return (
         <>
             <div className="wrapper">
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  commonjsOptions: {
-    esmExternals: true
-  },
-  server: {
-    port: 3001,
-  },
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    commonjsOptions: {
+      esmExternals: true
+    },
+    server: {
+      port: parseInt(process.env.VITE_PORT),
+    },
+  });
+}
+
